2341dfa68e7231cb5fe57df2681b698c2f018ed9
trieu02 <buiquoctrieu020402@gmail.com>
2025-05-18 02:54:20 +0700

    finish spawn and move the ground

---
+++ TheKnightAndTheNut/assets/Scripts/Railway/Railway.ts
@@ -0,0 +1,11 @@
+import { _decorator, Component, Node, Quat, UITransform, Vec3 } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("Railway")
+export class Railway extends Component {
+  init() {}
+
+  start() {}
+
+  update(deltaTime: number) {}
+}
