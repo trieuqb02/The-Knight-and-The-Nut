ea2e77f8ecbd24b08fa804600d76c9aad1a28cb8
FunGi2410 <nguyennhanit2410@gmail.com>
2025-05-18 23:07:39 +0700

    player on rail

--- TheKnightAndTheNut/assets/Scripts/Enemy/EnemyCtrl.ts
+++ TheKnightAndTheNut/assets/Scripts/Enemy/EnemyCtrl.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Collider2D, Component, Node, RigidBody2D, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('EnemyCtrl')
@@ -6,13 +6,17 @@ export class Enemy extends Component {
     @property
     speed: number = 15; 
 
-
     update(deltaTime: number) {
+        this.move(deltaTime);
+    }
+
+    move(deltaTime){
         this.node.setPosition(
             this.node.position.x - this.speed * deltaTime,
             this.node.position.y,
             this.node.position.z
         );
+        this.node.getComponent(Collider2D)?.apply();
     }
 }
 
